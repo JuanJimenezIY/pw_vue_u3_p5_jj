3d20b2bd776d9c4fe2aff792d8bf75f568e39827
Juan Jimenez <sr.j_599@hotmail.com>
2024-02-14 20:56:03 -0500

    taller 34

---
+++ src/components/Estudiante.vue
@@ -0,0 +1,52 @@
+<template>
+  <h1>Componente Estudiante</h1>
+  <input v-model="id" type="text">
+  <input v-model="nombre" type="text">
+  <button @click="consultarPorId">Consultar</button>
+   <button @click="insertar">Insertar</button>
+    <input  v-model="apellido" type="text">
+
+</template>
+
+<script>
+import {consultarEstudianteFachada,insertarFachada} from "../helpers/clienteEstudiante.js"
+
+export default {
+
+    data(){
+        return{
+            id:null,
+            nombre:null,
+            apellido:null
+        }
+    },
+    methods:{
+        async consultarPorId(){
+           const data= await consultarEstudianteFachada(this.id);
+           console.log("desde componente");
+           console.log(data);
+           this.nombre=data.nombre;
+        },
+       async insertar(){
+    const estuBody ={
+        nombre: "Juan",
+        apellido: this.apellido,
+        genero: "M",
+        fechaNacimiento: "1996-04-01T00:00:00",
+        correo: "edison@hotmail.com",
+        telefono: "2051651",
+        carrera: "Computacion",
+        semestre: "Decimo",
+        creditos: 19
+    };
+
+           await insertarFachada(estuBody);
+        }
+
+    }
+}
+</script>
+
+<style>
+
+</style>
